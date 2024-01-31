c7f45447fb892602e3c3d71a541667a3a2513ded
monoceros69 <dmago1607@gmail.com>
2024-01-31 22:35:07 +0100

    Removed unnecessary import

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.news.Models.NewsApiResponse;
